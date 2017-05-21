32682516ae41828a8424ec68bf05bba863551252
Colin Thompson <Colin Thompson>
2017-05-21 15:24:56 -0400

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+// This is the Link API
+import Link from 'next/link'
+
+
 const Index = () => (
   <div>
+    <Link href="/about">
+      <button>Go to About Page</button>
+    </Link>
     <p>Hello Next.js</p>
   </div>
 )
